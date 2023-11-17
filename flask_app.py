a447221d499cd28b6ec4b5c3f9fb602b768d9e39
turtle92 <rachekzyco@gmail.com>
2023-11-17 09:59:58 +0000

    Added html template, and made venv (not used yet)

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,26 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="turtle92",
+    password="turtlesareawesome",
+    hostname="turtle92.mysql.pythonanywhere-services.com",
+    databasename="turtle92$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
